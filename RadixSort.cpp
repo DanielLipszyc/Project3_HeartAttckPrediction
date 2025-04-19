e917f12f8bbe75a877c390157ddb972c7f012b81
MarianIMM <marian.irantzu@gmail.com>
2025-04-19 13:29:12 -0500

    New Updata RadixSort

--- RadixSort.cpp
+++ RadixSort.cpp
@@ -1,5 +1,50 @@
 //
-// Created by maria on 4/19/2025.
+// Created by Daniel Lipszyc on 4/19/25.
 //
 
 #include "RadixSort.h"
+
+// C++ implementation of Radix Sort
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int getMax(vector<Patient*> P) {
+    int max = P[0]->BMI;
+    for (int i = 1; i < P.size(); i++){
+        if (P[i]->BMI > max){
+            max = P[i]->BMI;
+        }
+    }
+    return max;
+}
+
+void countingSort(vector<Patient*>& patients, int exp) {
+    int n = patients.size();
+    vector<Patient*> output(n);
+    vector<int> count(10, 0);
+
+    for (const auto& p : patients)
+        count[(p->BMI / exp) % 10]++;
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        int index = (patients[i]->BMI / exp) % 10;
+        output[count[index] - 1] = patients[i];
+        count[index]--;
+    }
+
+    patients = output;
+}
+
+void radixSort(vector<Patient*>& patients) {
+    int maxBMI = getMax(patients);
+    for (int exp = 1; maxBMI / exp > 0; exp *= 10)
+        countingSort(patients, exp);
+}
