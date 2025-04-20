8a862a14ec5704c05fee373a0398b1be7db731de
Ivana Morales-Martinez <ivana.yilini@gmail.com>
2025-04-20 08:49:34 -0500

    Add interface with sfml

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
-using namespace std;
-
+#include <SFML/Graphics.hpp>
 #include "MergeSort.h"
 
+using namespace std;
 
 void extractBMIsFromFile(vector<Patient*> &list) {
     ifstream file("../heart_attack_dataset.csv");
@@ -58,11 +58,22 @@ void extractBMIsFromFile(vector<Patient*> &list) {
     file.close();
 }
 
-int main(){
+
+string RadixSort(int value) {
+    //Add code for Radix Sort
+
+    return "Value Radix";
+    //Return string
+}
+
+
+
+string MergeSort(int value) {
+    //Add code for Merge Sort
     int num = 1;
 
     vector<Patient*> Patients = {};
-    extractBMIsFromFile(Patients);
+    //extractBMIsFromFile(Patients);
     time_t start, end;
     time(&start);
 
@@ -73,7 +84,7 @@ int main(){
 
     int input_BMI;
     cout << "Enter a number between 20 and 40: ";
-    cin >> input_BMI;
+    //cin >> input_BMI;
 
 
     for(Patient* p : Patients){
@@ -84,4 +95,150 @@ int main(){
     }
 
     cout << difftime(end, start) << endl;
+
+
+    return "VAlue Merge";
+    //Return string
+}
+
+int main() {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 500}), "SFML window", sf::Style::Titlebar | sf::Style::Close);
+    //Set main banner
+    sf::Font fontTitle;
+    fontTitle.loadFromFile("C:/Windows/Fonts/arial.ttf");
+    sf::Text textTitle; // a font is required to make a text object
+    textTitle.setFont(fontTitle);
+    textTitle.setString("Merge Sort vs Radix Sort");
+    textTitle.setCharacterSize(30); // in pixels, not points!
+    textTitle.setFillColor(sf::Color::Blue);
+    textTitle.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    textTitle.setPosition((window.getSize().x / 2)- 190, 50);
+
+
+    //Enter Value Label
+    sf::Font fontLabel;
+    fontLabel.loadFromFile("C:/Windows/Fonts/arial.ttf");
+    sf::Text textLabel; // a font is required to make a text object
+    textLabel.setFont(fontLabel);
+    textLabel.setString("Enter Value: ");
+    textLabel.setCharacterSize(20); // in pixels, not points!
+    textLabel.setFillColor(sf::Color::Blue);
+    textLabel.setStyle(sf::Text::Bold);
+    textLabel.setPosition(150, 200);
+
+    //Enter Value Text
+    sf::Font fontInput;
+    fontInput.loadFromFile("C:/Windows/Fonts/arial.ttf");
+    sf::Text textInput; // a font is required to make a text object
+    textInput.setFont(fontInput);
+    textInput.setString("12");
+    textInput.setCharacterSize(20); // in pixels, not points!
+    textInput.setFillColor(sf::Color::Blue);
+    textInput.setPosition(300, 200);
+
+    //Result Label
+    sf::Font fontResultLabel;
+    fontResultLabel.loadFromFile("C:/Windows/Fonts/arial.ttf");
+    sf::Text textResultLabel; // a font is required to make a text object
+    textResultLabel.setFont(fontResultLabel);
+    textResultLabel.setString("Result Value: ");
+    textResultLabel.setCharacterSize(20); // in pixels, not points!
+    textResultLabel.setFillColor(sf::Color::Black);
+    textResultLabel.setStyle(sf::Text::Bold);
+    textResultLabel.setPosition(100, 420);
+
+    //Result Label
+    sf::Font fontResultValue;
+    fontResultValue.loadFromFile("C:/Windows/Fonts/arial.ttf");
+    sf::Text textResultValue; // a font is required to make a text object
+    textResultValue.setFont(fontResultValue);
+    textResultValue.setCharacterSize(20); // in pixels, not points!
+    textResultValue.setFillColor(sf::Color::Black);
+    textResultValue.setStyle(sf::Text::Bold);
+    textResultValue.setPosition(240, 420);
+
+
+    //Radix Button
+    sf::Texture radixTexture;
+    radixTexture.loadFromFile("../btnradixsort.png");
+    sf::Sprite radixSprite(radixTexture);
+    radixSprite.setPosition(sf::Vector2f(100,300));
+
+    //Merge Button
+    sf::Texture mergeTexture;
+    mergeTexture.loadFromFile("../btnmergesort.png");
+    sf::Sprite mergeSprite(mergeTexture);
+    mergeSprite.setPosition(sf::Vector2f(468,300));
+
+    string input_text;
+    string output_text;
+    // Start the game loop
+    sf::Clock clock;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if ((mousePos.x >= 100 ) && (mousePos.x <= 332 )
+                        && (mousePos.y >= 300 ) && (mousePos.y <= 382 )) {
+                        output_text =RadixSort(stoi(input_text));
+                        textResultValue.setString(output_text);
+                        }
+                    if ((mousePos.x >= 468 ) && (mousePos.x <= 700 )
+                        && (mousePos.y >= 300 ) && (mousePos.y <= 382 )) {
+                        output_text =MergeSort(stoi(input_text));
+                        textResultValue.setString(output_text);
+                        }
+                }
+            }
+            if (event.type == sf::Event::TextEntered) {
+                if (std::isprint(event.text.unicode))
+                    input_text += event.text.unicode;
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::BackSpace) {
+                    if (!input_text.empty())
+                        input_text.pop_back();
+                }
+                if (event.key.code == sf::Keyboard::Return) {
+                    input_text += '\n';
+                }
+            }
+        }
+
+        static sf::Time text_effect_time;
+        static bool show_cursor;
+
+        text_effect_time += clock.restart();
+
+        if (text_effect_time >= sf::seconds(0.5f))
+        {
+            show_cursor = !show_cursor;
+            text_effect_time = sf::Time::Zero;
+        }
+
+        textInput.setString(input_text + (show_cursor ? '_' : ' '));
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.clear(sf::Color::White);
+        window.draw(textTitle);
+        window.draw(textLabel);
+        window.draw(textInput);
+        window.draw(radixSprite);
+        window.draw(mergeSprite);
+        window.draw(textResultLabel);
+        window.draw(textResultValue);
+        window.display();
+    }
+    return 0;
 }
