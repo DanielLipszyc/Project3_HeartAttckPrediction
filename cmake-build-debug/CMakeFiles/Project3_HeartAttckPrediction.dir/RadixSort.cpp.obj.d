76bc5aa2234796f8d7890d1a3b23d9da59e9fd82
Ivana Morales-Martinez <ivana.yilini@gmail.com>
2025-04-20 09:03:13 -0500

    Add interface with sfml

---
+++ cmake-build-debug/CMakeFiles/Project3_HeartAttckPrediction.dir/RadixSort.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Project3_HeartAttckPrediction.dir/RadixSort.cpp.obj: \
+ C:\Git\Project3_HeartAttckPrediction\RadixSort.cpp \
+ C:\Git\Project3_HeartAttckPrediction\RadixSort.h
---
+++ cmake-build-debug/CMakeFiles/Project3_HeartAttckPrediction.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/MergeSort.cpp.obj"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/MergeSort.cpp.obj.d"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/PatientObject.cpp.obj"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/PatientObject.cpp.obj.d"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/RadixSort.cpp.obj"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/RadixSort.cpp.obj.d"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/main.cpp.obj"
+  "CMakeFiles/Project3_HeartAttckPrediction.dir/main.cpp.obj.d"
+  "Project3_HeartAttckPrediction.exe"
+  "Project3_HeartAttckPrediction.exe.manifest"
+  "Project3_HeartAttckPrediction.pdb"
+  "libProject3_HeartAttckPrediction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project3_HeartAttckPrediction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
